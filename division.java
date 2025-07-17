8be3bfa77c18e09911d7272f67505f261f57fe8e
VaibhavDev-sketch <vaibhavsagekar555@gmail.com>
2025-07-17 21:09:17 +0530

    didi

---
+++ division.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class division {
+    public static void main(String args[]) {
+        int a = 20;
+        int b = 4;
+        int result = a / b;
+        System.out.println("The division result is: " + result);
+    }
+}
